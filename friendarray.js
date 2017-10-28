2483988592fd7fae2573c48aa6980ca629ea006a
Andy Medina <deafjammer@yahoo.com>
2017-10-28 17:02:36 -0600

    yes

---
+++ friendarray.js
@@ -0,0 +1,5 @@
+var friendArray = [
+{name: "Olga", photo: "http://www.themakeupgallery.info/images/character/plain/fugly/dodge2.jpg", scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+]
+
+module.exports = friendArray;
